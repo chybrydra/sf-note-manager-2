a4fe5397c0e891ecd1169202316dba0c552d2e7d
chybrydra <lukasz.grymulski@gmail.com>
2019-11-26 10:17:52 +0100

    create logic to save new note

--- force-app/main/default/aura/NotesGenerator/NotesGeneratorController.js
+++ force-app/main/default/aura/NotesGenerator/NotesGeneratorController.js
@@ -1,5 +1,13 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    clickCreateNewNote: function(component, event, helper) {
+        var validNote = component.find('noteform').reduce(function (validSoFar, inputCmp) {
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        if(validNote){
+            var newNote = component.get("v.newNote");
+            console.log("Create note: " + JSON.stringify(newNote));
+            helper.createNote(component, newNote);
+        }
+    },
 })
--- force-app/main/default/aura/NotesGenerator/NotesGeneratorHelper.js
+++ force-app/main/default/aura/NotesGenerator/NotesGeneratorHelper.js
@@ -1,5 +1,18 @@
 ({
-    helperMethod : function() {
-
+    createNote: function(component, newNote) {
+        var action = component.get("c.saveNewNote");
+        action.setParams({
+            "newNote": newNote
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                Console.log('SUCCESS!!!!');
+                // var expenses = component.get("v.expenses");
+                // expenses.push(response.getReturnValue());
+                // component.set("v.expenses", expenses);
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
